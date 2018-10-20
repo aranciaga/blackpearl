c6ea091170c63ef383d458df5a3ba0e31110eece
aranciaga <aranciagami@gmail.com>
2018-10-20 16:40:53 -0300

    refactoring and fix subtitles api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var subs     = require('./lib/subs.js');
 var tpb      = require('./lib/tpb.js');
 var cli      = require('./lib/cli.js');
 var fs       = require('fs');
-var chalk    = require('chalk');
+var chalk	 = require('chalk');
 var action   = process.argv[2];
 var name     = process.argv[3];
 var actions  = ["search"];
@@ -19,17 +19,17 @@ var help_screen = function(){
 	  	console.log(chalk.yellow(data));
 		console.log(chalk.cyan("Usage: blackpearl search [name]"));
 		console.log(chalk.red("Subtitles: --sub [spa,eng,fre,etc]"));
-		process.exit(); 
+		process.exit() 
 	}); 
 	
 }
 
 if( actions.indexOf(action) == -1){
-	return help_screen();
+	return help_screen() 
 }
 
 if ( process.argv.indexOf("--sub") != -1 && process.argv[ process.argv.indexOf("--sub") + 1 ] ) {
-	subs_language = process.argv[ process.argv.indexOf("--sub") + 1 ];
+	subs_language = process.argv[ process.argv.indexOf("--sub") + 1 ] 
 }
 
 async.waterfall([
